1f0d2ee2b68763c730fc4716892bb12b590559f8
Duarte Monteiro <du5rte@users.noreply.github.com>
2017-08-26 21:31:27 +0100

    modify processRequest to work with streams

--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
 import mkdirp from 'mkdirp'
 import formidable from 'formidable'
-import objectPath from 'object-path'
+import objectPath, { get, set } from 'object-path'
 
-export function processRequest(request, { uploadDir } = {}) {
-  // Ensure provided upload directory exists
-  if (uploadDir) mkdirp.sync(uploadDir)
+// References
 
-  const form = formidable.IncomingForm({
-    // Defaults to the OS temp directory
-    uploadDir
-  })
+// Anatomy of a multipart form
+// http://derpturkey.com/node-multipart-form-data-explained/
+
+// Comparisson of multipart form parsers
+// https://stackoverflow.com/questions/23717879/how-could-i-upload-files-in-expressjs-4-x/23718166#23718166
+
+// How formidable Parses file uploads at 500 mb/s
+// http://debuggable.com/posts/parsing-a-form-in-node-js-1:4b0bff13-4244-4ebc-8455-4975cbdd56cb
+
+export function processRequest(request, options = {}) {
+  const form = formidable.IncomingForm(options)
+
+  // customize the multipart stream
+  // handle fields that are not files
+  // skip handle files stream
+  form.onPart = function(part) {
+    if (!part.filename) {
+      // let formidable handle all non-file parts
+      form.handlePart(part)
+    } else {
+      // emit files as stream to this.parse
+      this.emit('file', part.name, part)
+    }
+  }
 
   // Parse the multipart form request
   return new Promise((resolve, reject) => {
-    form.parse(request, (error, { operations }, files) => {
-      if (error) reject(new Error(error))
+    form.parse(request, (error, fields, files) => {
+      try {
+        if (error) throw error
 
-      // Decode the GraphQL operation(s). This is an array if batching is
-      // enabled.
-      operations = JSON.parse(operations)
+        // Decode the GraphQL operation(s). This is an array if batching is
+        // enabled.
+        const operations = JSON.parse(fields.operations)
 
-      // Check if files were uploaded
-      if (Object.keys(files).length) {
-        // File field names contain the original path to the File object in the
-        // GraphQL operation input variables. Relevent data for each uploaded
-        // file now gets placed back in the variables.
-        const operationsPath = objectPath(operations)
-        Object.keys(files).forEach(variablesPath => {
-          const { name, type, size, path } = files[variablesPath]
-          operationsPath.set(variablesPath, { name, type, size, path })
-        })
+        // Check if files were uploaded
+        if (Object.keys(files).length) {
+          // File field names contain the original path to the File object in the
+          // GraphQL operation input variables. Relevent data for each uploaded
+          // file now gets placed back in the variables.
+          Object.entries(files).forEach(([fieldname, file]) => {
+            // pull the metadata sent with the operation within variables
+            const meta = get(operations, fieldname)
+
+            if (meta) {
+              // add metadata do file
+              set(file, 'size', meta.size)
+              set(file, 'headers.length', meta.size)
+              set(file, 'name', file.filename)
+              set(file, 'type', file.mime)
+
+              // overwrite variable metadata with file stream
+              set(operations, fieldname, file)
+            }
+          })
+        }
+
+        // Provide fields for replacement request body
+        resolve(operations)
+      } catch (error) {
+        reject(error)
       }
-
-      // Provide fields for replacement request body
-      resolve(operations)
     })
   })
 }
